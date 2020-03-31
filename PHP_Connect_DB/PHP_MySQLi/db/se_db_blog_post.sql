3a28519491eba137809c233a198373d5154ae699
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2020-03-31 22:31:30 +0700

    [DB] Connect DB - Part 01

---
+++ PHP_Connect_DB/PHP_MySQLi/db/se_db_blog_post.sql
@@ -0,0 +1,3 @@
+INSERT INTO se_db_blog.post (id, content, account_id) VALUES (1, 'Hôm nay trời đẹp quá', 1);
+INSERT INTO se_db_blog.post (id, content, account_id) VALUES (2, 'Học lập trình PHP với thầy MR Bưởi', 1);
+INSERT INTO se_db_blog.post (id, content, account_id) VALUES (3, 'Hello Class', 2);
