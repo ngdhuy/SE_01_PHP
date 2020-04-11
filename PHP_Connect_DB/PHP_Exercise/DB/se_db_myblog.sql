1a684c747847d7b72b169e1b1655e13e86fb8fa9
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2020-04-11 21:31:27 +0700

    [DB] Exercise

---
+++ PHP_Connect_DB/PHP_Exercise/DB/se_db_myblog.sql
@@ -0,0 +1,30 @@
+create table account
+(
+    acc_id       int auto_increment
+        primary key,
+    username     varchar(255)                           not null,
+    password     varchar(64)                            not null,
+    display_name varchar(255)                           not null,
+    email        varchar(255)                           null,
+    is_active    tinyint(1) default 1                   null,
+    created_at   timestamp  default current_timestamp() not null,
+    updated_at   timestamp  default current_timestamp() not null
+);
+
+create table blog
+(
+    blog_id      int auto_increment
+        primary key,
+    acc_id       int                                   not null,
+    blog_content varchar(1024)                         null,
+    created_at   timestamp default current_timestamp() not null,
+    updated_at   timestamp default current_timestamp() not null,
+    constraint blog_ibfk_1
+        foreign key (acc_id) references account (acc_id)
+            on delete cascade
+);
+
+create index acc_id
+    on blog (acc_id);
+
+
